7855d6e021e53d5de6a0009d90112ae5f50a65f5
vjy <vjy@techflic.com>
2025-04-30 19:39:28 +0530

    updated context

--- src/@core/constants.js
+++ src/@core/constants.js
@@ -1,12 +1,14 @@
 const { CONST = {} } = window;
 
 export const APP = CONST.APP || "pushapp";
-export const APP_CONTEXT = {
-  pushapp: `/pushapp` || ``,
-  // pushapp: `/__dynamic_base__` || ``,
-  insights360: `/insights360` || ``,
-  notebook: `/notebook` || ``,
-}[APP];
+export const APP_CONTEXT =
+  (CONST.APP_CONTEXT == "/www" ? undefined : CONST.APP_CONTEXT) ||
+  {
+    pushapp: `/pushapp` || ``,
+    // pushapp: `/__dynamic_base__` || ``,
+    insights360: `/nexus/insights360` || ``,
+    notebook: `/nexus/notebook` || ``,
+  }[APP];
 export const REMOTE_SERVER_URL =
   CONST.remoteServerUrl || "https://demo.mehery.xyz";
 export const REMOTE_JS_URL = CONST.remoteJsUrl;
