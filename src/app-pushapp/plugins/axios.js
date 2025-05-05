e942b46d64a1a5c419313f657c2eabdb73f67053
vjy <vjy@techflic.com>
2025-05-05 13:39:51 +0530

    debug

--- src/app-pushapp/plugins/axios.js
+++ src/app-pushapp/plugins/axios.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const axiosIns = axios.create({
   // You can add your headers here
   // ================================
-  // baseURL: "",
+  baseURL: window.location.origin,
   // timeout: 1000,
   // headers: {'X-Custom-Header': 'foobar'}
 });
