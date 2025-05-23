0876dd9a8fb0d3b737c71e415eee482de013116f
vjy <vjy@techflic.com>
2025-05-23 11:59:03 +0530

    title and logo fix

--- themeConfig.js
+++ themeConfig.js
@@ -1,16 +1,22 @@
 import { breakpointsVuetify } from '@vueuse/core'
 import { VIcon } from 'vuetify/components/VIcon'
 
-// ❗ Logo SVG must be imported with ?raw suffix
-import logo from '@app/assets/images/logo.svg?raw'
 import { defineThemeConfig } from '@core'
 import { RouteTransitions, Skins } from '@core/enums'
 import { AppContentLayoutNav, ContentWidth, FooterType, NavbarType } from '@layouts/enums'
 
+// ❗ Logo SVG must be imported with ?raw suffix
+import logo from '@app/assets/images/logo.svg?raw'
+import logo_default from '@app/assets/images/logo_default.svg?raw'
+
+let app = window?.CONST?.WEBAPP?.split("/").pop() || window?.CONST?.APP
+let appTitle = app
+let appLogo = app === 'pushapp' ? logo : logo_default
+
 export const { themeConfig, layoutConfig } = defineThemeConfig({
   app: {
-    title: 'PushApp',
-    logo: h('div', { innerHTML: logo, style: 'line-height:0; color: rgb(var(--v-global-theme-primary))' }),
+    title: appTitle,
+    logo: h('div', { innerHTML: appLogo, style: 'line-height:0; color: rgb(var(--v-global-theme-primary))' }),
     contentWidth: ContentWidth.Boxed,
     contentLayoutNav: AppContentLayoutNav.Vertical,
     overlayNavFromBreakpoint: breakpointsVuetify.md + 16,
