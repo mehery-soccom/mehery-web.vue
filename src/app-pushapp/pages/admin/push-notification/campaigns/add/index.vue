dcab357229f47263cf14512d54f4768bbe09dabb
vjy <vjy@techflic.com>
2025-05-28 20:18:30 +0530

    cleanup

--- src/app-pushapp/pages/admin/push-notification/campaigns/add/index.vue
+++ src/app-pushapp/pages/admin/push-notification/campaigns/add/index.vue
@@ -1,60 +1,39 @@
 <script setup>
-import { useChannelsStore } from "@app/views/admin/channels/useChannelsStore";
 // import NotificationPreview from "@app/views/admin/push-notification/NotificationPreview.vue";
+import { useChannelsStore } from "@app/views/admin/channels/useChannelsStore";
 import { usePushNotificationStore } from "@app/views/admin/push-notification/usePushNotificationStore";
+const { show } = inject("snackbar");
 
 const route = useRoute();
 const router = useRouter();
-const pushNotificationStore = usePushNotificationStore();
 const channelsStore = useChannelsStore();
-const { show } = inject("snackbar");
+const pushNotificationStore = usePushNotificationStore();
+
+const tab = ref("tab-details");
 const isLoading = ref(false);
-
-const DEFAULT_IMAGE_URL = `https://media.licdn.com/dms/image/v2/D4D22AQFUmh8m0Xg9Iw/feedshare-shrink_800/B4DZX.a7mYH4Ag-/0/1743730228531?e=1748476800&v=beta&t=836J66x0qjuiwEVD7ZgCUCxLm8z7QI5JI3qD6y_4ROY`;
-const DEFAULT_LOGO_URL = `https://cdn.jsdelivr.net/gh/mehery-soccom/mehery-content@main/static/app/logo/bg-x-icon.png`;
-
-const form = reactive({
+const notification = reactive({
   channel_id: null,
   platforms: [],
-  title: "",
-  message: "",
-  image_url: DEFAULT_IMAGE_URL,
-  logo_url: DEFAULT_LOGO_URL,
-  buttonGroup: null,
-  buttonGroupValue: {},
 });
-const platform = ref("ios");
-const view = ref("collapse");
-const tab = ref("tab-details");
 const ChannelList = ref([]);
 const TemplateList = ref([]);
 
 onMounted(async () => {
-  let res = await channelsStore.fetchChannels();
-  ChannelList.value = res.results.map((c) => ({
-    label: c.channel_name || c.channel_id,
-    value: c.channel_id,
-  }));
+  let channelsRes = await channelsStore.fetchChannels();
+  ChannelList.value = channelsRes.results;
 
-  let templateRes = await pushNotificationStore.fetchTemplates();
-  TemplateList.value = templateRes.results;
+  let templatesRes = await pushNotificationStore.fetchTemplates();
+  TemplateList.value = templatesRes.results;
 
   const copy = route.query.copy;
   if (copy) {
     pushNotificationStore
       .fetchSimpleNotification({ id: copy })
       .then((response) => {
-        console.log(response.data.notification);
-        const notification = response.data.notification;
-        let _buttonGroupValue = {};
-        notification.buttons.map((b) => {
-          _buttonGroupValue[b.button_text] = b.button_url;
-        });
-        Object.assign(form, {
-          ...form,
+        const _notification = response.data.notification;
+        Object.assign(notification, {
           ...notification,
-          buttonGroup: notification.category,
-          buttonGroupValue: _buttonGroupValue,
+          ..._notification,
         });
       })
       .catch((error) => {
@@ -64,41 +43,23 @@ onMounted(async () => {
   }
 });
 
-const buttonGroupFields = computed(() => {
-  if (form.buttonGroup) {
-    return (
-      pushNotificationStore.buttonGroupList.find(
-        (b) => b.value === form.buttonGroup
-      )?.fields || []
-    );
-  }
-  return [];
-});
-
 const onSend = async () => {
   try {
     isLoading.value = true;
 
-    let template = TemplateList.value.find((t) => t._id === form.template);
+    let template = TemplateList.value.find(
+      (t) => t._id === notification.template
+    );
 
     let payload = {
       ...template.style,
       buttons: template.options.buttons,
-      // title: form.title,
-      // message: form.message,
-      // image_url: form.image_url,
-      // category: form.buttonGroup,
-      // buttons: buttonGroupFields.value.map((b) => ({
-      //   button_id: b.id,
-      //   button_text: b.text,
-      //   button_url: form.buttonGroupValue[b.text],
-      // })),
 
       filter: {
-        platform: form.platforms.join(" "),
+        platform: notification.platforms.join(" "),
         session_type: "all",
       },
-      channel_id: form.channel_id,
+      channel_id: notification.channel_id,
     };
 
     await pushNotificationStore.sendBulk(payload);
@@ -114,10 +75,6 @@ const onSend = async () => {
     isLoading.value = false;
   }
 };
-
-watch(platform, (newValue) => {
-  view.value = "collapse";
-});
 </script>
 
 <template>
@@ -146,7 +103,7 @@ watch(platform, (newValue) => {
                   <VRow>
                     <VCol cols="12" md="6">
                       <AppTextField
-                        v-model="form.name"
+                        v-model="notification.name"
                         label="Notification Name"
                         placeholder="Enter Notification Name"
                       />
@@ -154,67 +111,23 @@ watch(platform, (newValue) => {
 
                     <VCol cols="12" md="6">
                       <AppSelect
-                        v-model="form.template"
+                        v-model="notification.template"
                         :items="TemplateList"
                         label="Template"
                         placeholder="Select a template"
                         item-title="code"
                         item-value="_id"
                         clearable
-                      />
+                      >
+                        <template #item="{ props, item }">
+                          <v-list-item v-bind="props" class="px-4">
+                            <div class="dropdown-option-meta text-caption">
+                              {{ item.raw.type }}
+                            </div>
+                          </v-list-item>
+                        </template>
+                      </AppSelect>
                     </VCol>
-
-                    <!-- <VCol cols="12" md="12">
-                      <AppTextField
-                        v-model="form.title"
-                        label="Title"
-                        placeholder="Enter Notification Title"
-                      />
-                    </VCol>
-
-                    <VCol cols="12" md="12">
-                      <AppTextarea
-                        v-model="form.message"
-                        label="Message"
-                        placeholder="Enter Notification Message"
-                      />
-                    </VCol>
-
-                    <VCol cols="12" md="12">
-                      <AppTextField v-model="form.logo_url" label="Logo URL" />
-                    </VCol>
-
-                    <VCol cols="12" md="12">
-                      <AppTextField
-                        v-model="form.image_url"
-                        label="Image URL"
-                      />
-                    </VCol>
-
-                    <VCol cols="12" md="6">
-                      <AppSelect
-                        v-model="form.buttonGroup"
-                        :items="pushNotificationStore.buttonGroupList"
-                        label="CTA Group"
-                        placeholder="Select Button Group"
-                        item-title="label"
-                        item-value="value"
-                        clearable
-                      />
-                    </VCol>
-
-                    <VCol
-                      cols="12"
-                      md="12"
-                      v-if="buttonGroupFields.length"
-                      v-for="b in buttonGroupFields"
-                    >
-                      <AppTextField
-                        v-model="form.buttonGroupValue[b.text]"
-                        :label="'Button > ' + b.text"
-                        placeholder="Enter URL"
-                      />
-                    </VCol> -->
                   </VRow>
                 </VForm>
               </VWindowItem>
@@ -224,19 +137,19 @@ watch(platform, (newValue) => {
                   <VRow>
                     <VCol cols="12" md="6">
                       <AppSelect
-                        v-model="form.channel_id"
+                        v-model="notification.channel_id"
                         :items="ChannelList"
                         label="App"
                         placeholder="Select App"
-                        item-title="label"
-                        item-value="value"
+                        item-title="channel_name"
+                        item-value="channel_id"
                         clearable
                       />
                     </VCol>
 
                     <VCol cols="12" md="6">
                       <AppSelect
-                        v-model="form.platforms"
+                        v-model="notification.platforms"
                         :items="pushNotificationStore.platformList"
                         label="Platform"
                         placeholder="Select Platforms"
@@ -277,35 +190,16 @@ watch(platform, (newValue) => {
         </VCard>
       </v-card>
     </v-col>
-
-    <!-- Preview Column -->
-    <!-- <VCol cols="12" md="5">
-      <VRow>
-        <v-col cols="6" class="d-flex justify-center">
-          <v-btn-toggle v-model="platform" mandatory density="compact">
-            <v-btn value="ios">iOS</v-btn>
-            <v-btn value="android">Android</v-btn>
-          </v-btn-toggle>
-        </v-col>
-        <v-col cols="6">
-          <v-btn-toggle v-model="view" mandatory density="compact">
-            <v-btn value="collapse">Collapse</v-btn>
-            <v-btn value="expand">Expand</v-btn>
-          </v-btn-toggle>
-        </v-col>
-      </VRow>
-      <VRow>
-        <v-col cols="12" class="d-flex justify-center">
-          <NotificationPreview
-            :platform="platform"
-            :view="view"
-            :form="form"
-            :buttonGroupFields="buttonGroupFields"
-          />
-        </v-col>
-      </VRow>
-    </VCol> -->
   </v-row>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.dropdown-option-meta {
+  position: absolute;
+  top: 0;
+  right: 0;
+  padding: 0.5rem;
+  display: flex;
+  justify-content: center;
+}
+</style>
